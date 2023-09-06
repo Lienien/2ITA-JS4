fd6340550c403f3945f49bf4f66e65a4e4ab19db
Lienien <116272320+Lienien@users.noreply.github.com>
2023-09-06 10:20:15 +0200

    bing bada boom

---
+++ script.js
@@ -0,0 +1,46 @@
+//Oppgave 1
+/*
+let penis = 0
+
+while (penis <= 50) {
+    if (penis % 2 == 0) {
+        document.write(penis + "<br>")
+    }
+    penis++
+}
+*/
+
+//Opgave 2
+/*
+let penis = 0
+
+while (penis <= 50) {
+    if (penis % 5 == 0) {
+        document.write(penis + "<br>")
+    }
+    penis++
+}
+*/
+
+//Oppgave 3
+/*
+let penis = 0
+
+while (penis <= 100) {
+    if (penis % 5 == 0 && penis % 3 == 0) {
+        document.write(penis + "<br>")
+    }
+    penis++
+}
+*/
+
+//Oppgave 4
+
+
+
+//Oppgave 5
+
+
+
+//Oppgave 6
+
